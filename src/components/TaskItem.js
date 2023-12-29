ac0f4ff3acd57169f2fec59308c29a3e53e7a4bd
Irenendinda <irene.ndindaw@gmail.com>
2023-12-29 11:43:17 +0300

    Updated TaskItem Component

--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -10,9 +10,80 @@ function TaskItem({Task, DeleteTask, updateTask}) {
 
     const {id, title, priority, dueDate, category, status}= Task;
 
-  return (
-    <div>TaskItem</div>
-  )
+    const handleDelete = () => {
+        DeleteTask(id)
+    };
+
+    const handleEdit = () => {
+        setIsEditing(true)
+    };
+
+    const handleSave = () => {
+        updateTask(editedTask)
+        setIsEditing(false)
+    };
+
+    const handleCancel = () => {
+        setIsEditing(false)
+        setIsEditedTask({...Task})
+    }
+
+    const handleChange = (event) => {
+        const {name, value}= event.target
+        setIsEditedTask({...editedTask, [name]: value})
+    }
+
+    return (
+        <div className="task">
+          {isEditing ? (
+            <div>
+              <input
+                type="text"
+                name="title"
+                value={title}
+                onChange={handleChange}
+              />
+              <input
+                type="text"
+                name="priority"
+                value={priority}
+                onChange={handleChange}
+              />
+              <input
+                type="text"
+                name="dueDate"
+                value={dueDate}
+                onChange={handleChange}
+              />
+              <input
+                type="text"
+                name="category"
+                value={category}
+                onChange={handleChange}
+              />
+              <input
+                type="text"
+                name="status"
+                value={status}
+                onChange={handleChange}
+              />
+              <button onClick={handleSave}>Save</button>
+              <button onClick={handleCancel}>Cancel</button>
+            </div>
+          ) : (
+            
+            <div>
+              <h3>{title}</h3>
+              <p>{priority}</p>
+              <p>{dueDate}</p>
+              <p>{category}</p>
+              <p>{status}</p>
+              <button onClick={handleEdit}>Edit</button>
+              <button onClick={handleDelete}>Delete</button>
+            </div>
+          )}
+        </div>
+    )      
 }
 
 export default TaskItem
