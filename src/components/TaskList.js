ac67c05ae471c3387e90d2a901c43e4ff0f3a473
Benson Mwangi <kimaniben@gmail.com>
2024-01-01 10:57:01 -0500

    Debugging TaskList Component

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -2,6 +2,11 @@ import React from 'react';
 import TaskItem from './TaskItem';
 
 function TaskList({ tasks, deleteTask, updateTask }) {
+  // Check if tasks is undefined or null
+  if (!tasks) {
+    return null; // or you can return a loading indicator or an empty list
+  }
+
   return (
     <div className='taskList'>
       {tasks.map((task) => (
